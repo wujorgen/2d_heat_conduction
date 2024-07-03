e3bf9979c338b911f0c5db4fd5b0a2bd9dbb0f34
Jorgen Wu <wujorgen@gmail.com>
2024-07-03 01:21:07 -0400

    cleaned up coefficients in steady 1d file

--- C_CODE/src_1D/steady1d.c
+++ C_CODE/src_1D/steady1d.c
@@ -24,19 +24,20 @@ int main()
 	// delta_x = 0.1 Meters i guess
 	// Set boundary condition types to None
 	// Set initial guess for all temperatures to 0 (Celsius I think)
+	// Temperature initial guess doesn't really matter here since its steady state
 	for (int i = 0; i < N; i++)
 	{
 		Mesh[i].T = 20;
 		Mesh[i].k = 45.0;
 		Mesh[i].dx = 0.1;
-		//strcpy(Mesh[i].bc, "None");
+		// strcpy(Mesh[i].bc, "None");
 	}
 
 	// Heat flux boundary condition
-	//strcpy(Mesh[0].bc, "HeatFlux");
+	// strcpy(Mesh[0].bc, "HeatFlux");
 	double phi_bound = 100; // Watts / Square Meter
 	// Convection boundary condition
-	//strcpy(Mesh[N - 1].bc, "Convection");
+	// strcpy(Mesh[N - 1].bc, "Convection");
 	double T_amb = 22;
 	double h_amb = 25.32; // convection, steel-to-air, W / (m^2 K)
 
@@ -51,11 +52,19 @@ int main()
 	// Point-by-point
 	// Temperatures from previous iteration
 	double T_last[N];
+
 	// solution tolerance
 	double stol = 0.001;
 	bool ITERATE = true;
+
 	// residual tracker, L_inf or L2
 	double R;
+
+	// equation coefficient declarations
+	double a_p;
+	double a_w;
+	double a_e;
+
 	for (int iter = 0; ITERATE; iter++) // iterate
 	{
 		// store temps from last iteration
@@ -68,18 +77,27 @@ int main()
 		{
 			if (i == 0)
 			{
-				Mesh[0].T = h_mean(Mesh[0].k, Mesh[1].k) / Mesh[1].dx * Mesh[1].T + phi_bound;
-				Mesh[0].T /= h_mean(Mesh[0].k, Mesh[1].k) / Mesh[0].dx;
+				a_w = 0.0;
+				a_e = h_mean(Mesh[i].k, Mesh[i + 1].k) / Mesh[i].dx;
+				a_p = a_w + a_e;
+				Mesh[0].T = a_e * Mesh[1].T + phi_bound;
+				Mesh[0].T /= a_p;
 			}
 			else if (i == N - 1)
 			{
-				Mesh[i].T = h_mean(Mesh[i - 1].k, Mesh[i].k) / Mesh[i - 1].dx * Mesh[i - 1].T + h_amb * T_amb;
-				Mesh[i].T /= (h_mean(Mesh[0].k, Mesh[1].k) / Mesh[0].dx) + h_amb;
+				a_w = h_mean(Mesh[i - 1].k, Mesh[i].k) / Mesh[i].dx;
+				a_e = 0.0;
+				a_p = a_w + a_e + h_amb;
+				Mesh[i].T = a_w * Mesh[i - 1].T + h_amb * T_amb;
+				Mesh[i].T /= a_p; //(h_mean(Mesh[0].k, Mesh[1].k) / Mesh[0].dx) + h_amb;
 			}
 			else
 			{
-				Mesh[i].T = h_mean(Mesh[i - 1].k, Mesh[i].k) / Mesh[i - 1].dx * Mesh[i - 1].T + h_mean(Mesh[i].k, Mesh[i + 1].k) / Mesh[i + 1].dx * Mesh[i + 1].T;
-				Mesh[i].T /= h_mean(Mesh[i - 1].k, Mesh[i].k) / Mesh[i].dx + h_mean(Mesh[i].k, Mesh[i + 1].k) / Mesh[i].dx;
+				a_w = h_mean(Mesh[i - 1].k, Mesh[i].k) / Mesh[i].dx;
+				a_e = h_mean(Mesh[i].k, Mesh[i + 1].k) / Mesh[i].dx;
+				a_p = a_w + a_e;
+				Mesh[i].T = a_w * Mesh[i - 1].T + a_e * Mesh[i + 1].T;
+				Mesh[i].T /= a_p;
 			}
 		}
 		// check for convergence - I think this is basically L_inf
@@ -92,9 +110,10 @@ int main()
 			}
 		}
 		// print temps after point-by-point, every tenth iteration OR when converged
-		if (iter % 10 == 0 || !ITERATE)
+		if (iter % 20 == 0 || !ITERATE)
 		{
-			if (!ITERATE) printf("CONVERGED - SOLUTION BELOW:\n");
+			if (!ITERATE)
+				printf("CONVERGED - SOLUTION BELOW:\n");
 			printf("ITR %d: ", iter);
 			for (int i = 0; i < N; i++)
 			{
